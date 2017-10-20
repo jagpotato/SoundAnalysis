775763a86c95d73cdba9e0e2f494744bf4620193
s13t275 <dik.piyo.piyo@gmail.com>
2017-10-20 14:16:03 +0900

    commit

---
+++ Assets/Scripts/StartMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMusic : MonoBehaviour {
+	public AudioClip audioClip;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = gameObject.GetComponent<AudioSource>();
+		audioSource.clip = audioClip;
+		audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/WaveOutputter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(AudioSource))]
+public class WaveOutputter : MonoBehaviour {
+
+	private AudioSource audio;
+	private float[] waveData_ = new float[1024];
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		audio.GetOutputData(waveData_, 1);
+		var volume = waveData_.Select(x => x*x).Sum() / waveData_.Length;
+		transform.localScale = Vector3.one * volume * 30;
+	}
+}
